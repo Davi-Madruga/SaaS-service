2c69cbb8848f3855e5c9f230070e7ee5a0fa00dc
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 11:43:16 -0300

    Feat: Registro Serializer Cadastro Usuario

---
+++ apps/perfil/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Perfil
+from django.db import transaction
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class RegistroSerializer(serializers.Serializer):
+    nome = serializers.CharField()
+    telefone = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)    
+
+    def validate_email(self, value):
+        email = value.strip().lower()
+
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("Não foi possível realizar o cadastro")
+        
+        return email
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email = validated_data['email'],
+            password = validated_data['password']
+        )
+
+        perfil = Perfil.objects.create(
+            user = user,
+            nome = validated_data['nome'],
+            telefone = validated_data['telefone'],
+            tipo = 'cliente'
+        )
+        
+        return {
+            'id': perfil.id,
+            'nome': perfil.nome,
+            'telefone': perfil.telefone,
+            'tipo': perfil.tipo,
+            'email': user.email
+        }
