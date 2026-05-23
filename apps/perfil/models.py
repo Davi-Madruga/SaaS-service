3f67699cf06685dab1c8c5b54be0bc0be498dd02
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 14:50:55 -0300

    Fix: UsuarioManager para customizar usuario

--- apps/perfil/models.py
+++ apps/perfil/models.py
@@ -1,6 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("O email é obrigatório")
+        
+        email = self.normalize_email(email)
+        
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser precisa ter is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser precisa ter is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
 
 class Usuario(AbstractUser):
     username = None
@@ -8,6 +36,8 @@ class Usuario(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UsuarioManager()
 
 class Perfil(models.Model):
     TIPOS = [
@@ -20,6 +50,7 @@ class Perfil(models.Model):
     nome = models.CharField(max_length=120, verbose_name='Nome')
     telefone = models.CharField(max_length=20, verbose_name='Telefone')
     tipo = models.CharField(max_length=20,default='cliente', choices=TIPOS, verbose_name='Tipo do Perfil')
+    criado_em = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'Perfil'
