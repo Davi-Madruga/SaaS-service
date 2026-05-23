a9729424603acb16fbaeab09e443087c9064c80a
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 15:44:40 -0300

    Feat: ServicoViewSet com permissoes

--- apps/barbearia/views.py
+++ apps/barbearia/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Servico
+from .serializers import ServicoSerializer
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
+from .permissions import IsAdmin
 
-# Create your views here.
+class ServicoViewSet(viewsets.ModelViewSet):
+    queryset = Servico.objects.all()
+    serializer_class = ServicoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            return[IsAuthenticated()]
+        return[IsAuthenticated(),IsAdmin()]
