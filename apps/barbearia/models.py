2e6737325be14111b1bb5d7fc7de28ef109c7517
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 15:28:36 -0300

    Feat: Servico

--- apps/barbearia/models.py
+++ apps/barbearia/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Servico(models.Model):
+    nome = models.CharField(max_length=80,verbose_name="Nome")
+    valor = models.DecimalField(max_digits=6,decimal_places=2)
+
+    def __str__(self):
+        return self.nome
