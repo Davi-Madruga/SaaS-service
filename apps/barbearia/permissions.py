95a8824d011510c64f87983c1eb7e7b59820c0a2
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 18:06:04 -0300

    Feat: permissions

--- apps/barbearia/permissions.py
+++ apps/barbearia/permissions.py
@@ -1,14 +1,14 @@
 from rest_framework.permissions import BasePermission
-from django.core.exceptions import ObjectDoesNotExist
 
 class IsAdmin(BasePermission):
-    message = 'Apenas administradores podem gerenciar serviços.'
-
     def has_permission(self, request, view):
-        if not request.user or not request.user.is_authenticated:
+        if not request.user.is_authenticated:
             return False
 
-        try:
-            return request.user.perfil.tipo == 'admin'
-        except ObjectDoesNotExist:
-            return False
+        if request.user.is_superuser:
+            return True
+
+        return (
+            hasattr(request.user, "perfil")
+            and request.user.perfil.tipo == "admin"
+        )
