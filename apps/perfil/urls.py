2250a2bf4e25b7b0733bee1ca60677c26a12e205
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 15:17:58 -0300

    Feat: urls e tokens conectados

--- apps/perfil/urls.py
+++ apps/perfil/urls.py
@@ -1,7 +1,16 @@
+from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import CadastroClienteViewSet
+from .views import CadastroClienteViewSet, PerfilViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 
 router.register(r'clientes', CadastroClienteViewSet, basename='cliente')
-urlpatterns = router.urls
+router.register(r'perfis', PerfilViewSet, basename='perfil')
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
