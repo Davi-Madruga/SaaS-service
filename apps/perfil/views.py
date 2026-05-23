ca5d4fac1989b5ecee5e44fd1d55116d06c6ef84
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 15:17:18 -0300

    Feat: PerfilViewSet

--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -1,8 +1,23 @@
 from rest_framework import viewsets, mixins
-from .serializers import CadastroClienteSerializer
+from .serializers import CadastroClienteSerializer, PerfilSerializer
 from rest_framework.permissions import AllowAny
-
+from rest_framework.permissions import IsAuthenticated
+from .models import Perfil
 
 class CadastroClienteViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = CadastroClienteSerializer
     permission_classes = [AllowAny]
+
+class PerfilViewSet(mixins.ListModelMixin,mixins.UpdateModelMixin,
+mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    serializer_class = PerfilSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+
+        perfil = self.request.user.perfil
+
+        if perfil.tipo == 'admin':
+            return Perfil.objects.all()
+        
+        return Perfil.objects.filter(user=self.request.user)
