6f43c4fec89f3efe911ff3cb5e61090b692b9e59
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 20:30:18 -0300

    Feat: agendamentos urls

--- apps/barbearia/urls.py
+++ apps/barbearia/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import ServicoViewSet
+from .views import ServicoViewSet, AgendamentoViewSet
 
 router = DefaultRouter()
 
 router.register(r'servicos', ServicoViewSet, basename='servico')
+router.register(r"agendamentos", AgendamentoViewSet, basename="agendamento")
 
 urlpatterns = router.urls
