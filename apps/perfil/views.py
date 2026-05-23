f5c94ea3be8646bb3be5cd0479cfa8099b138dfa
Davi Madruga <davimadruga@outlook.com.br>
2026-05-23 11:56:01 -0300

    Feat: RegistroViewSet

--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from .serializers import RegistroSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class RegistroViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    serializer_class = RegistroSerializer
+    permission_classes = [AllowAny]
