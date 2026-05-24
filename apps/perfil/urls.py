7dae9f250b9a2c0e4043523a0aa84f50a5882a78
Davi Madruga <davimadruga@outlook.com.br>
2026-05-24 15:10:31 -0300

    Feat: Cadastro by admin urls

--- apps/perfil/urls.py
+++ apps/perfil/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CadastroClienteViewSet, PerfilViewSet
+from .views import CadastroClienteViewSet, PerfilViewSet, CadastroUsuarioAdminViewSet
 from django.urls import path,include
 
 router = DefaultRouter()
 
+router.register(r'usuarios', CadastroUsuarioAdminViewSet, basename="usuario")
 router.register(r'clientes', CadastroClienteViewSet, basename='cliente')
 router.register(r'perfis', PerfilViewSet, basename='perfil')
 
